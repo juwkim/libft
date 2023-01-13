c6d286a730f4f256117889c9a0488333274ec1d2
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-13 10:17:10 +0900

    Update ft_strncmp.c

--- ft_string/ft_strncmp.c
+++ ft_string/ft_strncmp.c
@@ -14,10 +14,10 @@
 
 int	ft_strncmp(const char *s1, const char *s2, int n)
 {
-	while (n-- && *s1 && *s1 != *s2)
+	while (n-- && *s1 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
