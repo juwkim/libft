532d345acfde57aed6ed2f80e7d22f60ba5b6f47
= <=>
2023-01-14 15:26:52 +0900

    ft_swap add

--- ft_math/ft_swap.c
+++ ft_math/ft_swap.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_math.h                                          :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/09 10:01:44 by juwkim            #+#    #+#             */
-/*   Updated: 2023/01/14 15:25:34 by juwkim           ###   ########.fr       */
+/*   Created: 2023/01/14 15:23:15 by juwkim            #+#    #+#             */
+/*   Updated: 2023/01/14 15:26:20 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_MATH_H
-# define FT_MATH_H
+#include "ft_math.h"
 
-# include "ft_is.h"
-# include "ft_memory.h"
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
 
-int		ft_min(int a, int b);
-int		ft_max(int a, int b);
-int		ft_median(int a, int b, int c);
-int		ft_abs(int a);
-void	ft_swap(int *a, int *b);
-
-int		ft_atoi(const char *str);
-char	*ft_itoa(int n);
-
-#endif
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
